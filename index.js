c959745a588d39b5d74141605276f4b97afc7c13
Your NameDevbruno08 <bruno_emonteiro@hotmail.com>
2022-08-30 00:47:38 -0300

    feat: update swagger

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const { config } = require('dotenv');
 const routesCharacters = require('./src/Routes/characters.routes');
 const routesUser = require('./src/Routes/user.routes');
 const routesAuth = require('./src/Routes/auth.routes');
-const connectToDatabase = require('./src/database/MongoDB/database')
+const connectToDatabase = require('./src/database/MongoDB/database');
 
 const app = express();
 
 connectToDatabase();
 config();
 
-const swagger = require("swagger-ui-express");
-const swaggerDocs = require("./src/docs/swagger.json");
-app.use("/docs", swagger.serve, swagger.setup(swaggerDocs));
+const swagger = require('swagger-ui-express');
+const swaggerDocs = require('./src/docs/swagger.json');
+app.use('/docs', swagger.serve, swagger.setup(swaggerDocs));
 
 app.use(express.json());
 app.use(cors());
@@ -23,5 +23,5 @@ app.use(routesAuth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
